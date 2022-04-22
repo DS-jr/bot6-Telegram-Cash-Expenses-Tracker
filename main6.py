47e071506830e5e95a9d69bc0393d8d3bd671425
DS-jr <93123740+DS-jr@users.noreply.github.com>
2022-04-22 10:51:23 +0300

    Update main6.py

--- main6.py
+++ main6.py
@@ -9,38 +9,38 @@ from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
 
 path6 = "..."  # Paste the path to your project folder 
 
-# Set scope for authentication (Google Drive & Google Sheets) 
+# Create a scope for authentication (Google Drive & Google Sheets):  
 scope6 = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive.file',
          'https://www.googleapis.com/auth/drive'] 
 
-# Authenticate using your credentials saved in JSON 
-creds6 = ServiceAccountCredentials.from_json_keyfile_name(path6 + "/NAME_OF_YOUR_JSON_FILE.json", scope6)  # Paste the name of your JSON file dowloaded when enabling Google Drive API 
+# Authenticate using your credentials saved in JSON: 
+creds6 = ServiceAccountCredentials.from_json_keyfile_name(path6 + "/NAME_OF_YOUR_JSON_FILE.json", scope6)  # Paste the name of your JSON file dowloaded when enabling Google Drive API & saved in your project folder
 
 bot6_token = "BOT_TOKEN"   # Substitute "BOT_TOKEN" phrase with your Bot's Token. Get it when registering a new bot via BotFather on Telegram.
 
-# Connect with Google Sheets
+# Connect with Google Sheets:
 client6 = gspread.authorize(creds6)
 my_sheet6 = client6.open("NAME_OF_YOUR_SPREADSHEET").sheet1   # Substitute "NAME_OF_YOUR_SPREADSHEET" phrase with the name of your Google Sheets file
 
-# Get data from the sheet as DataFrame
+# Get data from the sheet as DataFrame: 
 data6 = gspread_dataframe.get_as_dataframe(my_sheet6, parse_dates=True, usecols=[0,1,2,3,4])
 
 # My Google Sheet table has 4 columns: Date; Expenses; Category; Comments
-# Format columns 
+# Format columns: 
 data6 = data6.astype({"Expenses" : "float64"})
 data6.Date = pd.to_datetime(data6.Date)
 
 
-# Handle incoming messages and offer 'types' of expenses to choose from
+# Handle incoming messages and offer 'categories' of expenses to choose from:
 def enter_expenses(update, context):   
     input1 = update.message.text.split(",")   
     try:
         input1[1] = float(input1[1])
     except IndexError:
         return  
-    # Buttons with predefined categories of expenses 
+    # Buttons with predefined categories of expenses:  
     buttons = [[]]
     buttons.append([])
     buttons.append([])
@@ -52,10 +52,10 @@ def enter_expenses(update, context):
     bot6.send_message(update.message.chat_id, update.message.text, reply_markup=keyboard)
 
          
-# Save the expense to the Google Sheets table after the button in the bot is pushed:
+# Save the expense to the Google Sheets table after the button in the bot is pushed: 
 def callback_query_handler(update, context):
     bot6.delete_message(update.callback_query.message.chat_id, str(update.callback_query.message.message_id))  # (?!) (during testing) Try to delete this line & check what will be changed
-    # Refresh connection with Google Sheets and data:
+    # Refresh connection with Google Sheets and data: 
     client6 = gspread.authorize(creds6)
     my_sheet6 = client6.open("NAME_OF_YOUR_SPREADSHEET").sheet1   # Substitute "NAME_OF_YOUR_SPREADSHEET" phrase with the name of your Google Sheets file
     records6 = my_sheet6.get_all_records()
@@ -81,4 +81,3 @@ updater.dispatcher.add_handler(CallbackQueryHandler(callback_query_handler))
 # Start polling:
 updater.start_polling()
 updater.idle()
-
