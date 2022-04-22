fbcdd6340a0d0ee2fc4c527bcadb0f9b90933337
DS-jr <93123740+DS-jr@users.noreply.github.com>
2022-04-22 08:42:31 +0300

    Update main6.py

--- main6.py
+++ main6.py
@@ -1,1 +1,9 @@
+import os, sys
+import pandas as pd
+import gspread
+import gspread_dataframe
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
 
